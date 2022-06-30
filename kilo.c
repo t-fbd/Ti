5d958171923c44cf66e2ba80c5ba396c2b39a469
T Dunham <tairenfd@mailbox.org>
2022-06-30 17:07:34 -0400

    more flags added

--- kilo.c
+++ kilo.c
@@ -29,7 +29,7 @@ void disableRawMode () {
   
 }
 
-void enabeRawMode () {
+void enableRawMode () {
 
   /*
     Get user default termios and return to defaults at exit
@@ -43,6 +43,7 @@ void enabeRawMode () {
     struct termios raw = orig_termios;
   
   /*
+  local flags -
     Disable canonical mode with ICANON - input will be read byte-by-byte instead of
       line-by-line now
     Disable echo with ECHO 
@@ -52,13 +53,26 @@ void enabeRawMode () {
       and ctrl-q will be read as a '19' byte and '17' byte respectiely
     Disable ctrl-v / IEXTEN flag - ctrl-v read as a '22' byte now, and on mac
       ctrl-o will now be a '15' byte
-  
+  input flags - 
     Disable flags by setting flag (c_lflag ie. control local flag, c_iflag - input 
       flags), then set flag attributes to termios raw
-    
+    Disable translation of carriage returns ('13') to new lines('10') with ICRNL 
+      (input carriage return new line) - ctrl-m and enter now read as a '13' byte 
+      and not '10'
+  output flags - 
+    Disable post-processing of output with OPOST - \r\n neccesarry for a newline
+      now
+  miscellaneous flags -
+    includes BRKINT, INPCK, ISTRIP and CS8
+    refer to miscellaneous flag section of https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html
+      for some more info. Most of these flags shouldn't have nay noticable effect
+      in modern terminals, but switching them off apparently used to be commonplace 
+      when enabling 'raw mode'
   */
-  raw.c_iflag &= ~(IXON);
-  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN |ISIG);
+  raw.c_iflag &= ~( BRKINT | ICRNL | INPCK | ISTRIP | IXON );
+  raw.c_oflag &= ~( OPOST );
+  raw.c_cflag &= ~( CS8 );
+  raw.c_lflag &= ~( ECHO | ICANON | IEXTEN |ISIG );
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
     
 }
@@ -72,7 +86,7 @@ int main () {
     'General Terminal Interface - The Single UNIX ® Specification, Version 2'
     for info on canonical and raw input modes.
   */
-  enabeRawMode();
+  enableRawMode();
 
   /*
     Initialize char c to use as a buffer
@@ -91,12 +105,12 @@ int main () {
     */
     if (iscntrl(c)) {
       
-      printf("%d\n", c);
+      printf("%d\r\n", c);
       
     }
     else {
       
-      printf("%d (%c)\n", c, c);
+      printf("%d (%c)\r\n", c, c);
       
     }
     
