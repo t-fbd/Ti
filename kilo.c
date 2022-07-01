6bd07a4a0c9c944ded0e72f18bd9d693ec1fdbd0
T Dunham <tairenfd@mailbox.org>
2022-07-01 01:05:07 -0400

    del key added - no fnuctionality yet

--- kilo.c
+++ kilo.c
@@ -32,6 +32,7 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
   HOME_KEY,
   END_KEY,
   PAGE_UP,
@@ -208,6 +209,7 @@ int editorReadKey () {
           
           switch (seq[1]) {
             case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
             case '4': return END_KEY;
             // \x1b[5~ = page up
             case '5': return PAGE_UP;
