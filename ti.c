74258e026e5e29a93eae2491e1104dfee169e8ea
T Dunham <tairenfd@mailbox.org>
2022-07-05 04:25:53 -0400

    more syntax additions, non-printable chars

--- ti.c
+++ ti.c
@@ -51,6 +51,8 @@ enum editorHighlight {
 
   HL_NORMAL = 0,
   HL_COMMENT,
+  HL_KEYWORD1,
+  HL_KEYWORD2,
   HL_STRING,
   HL_NUMBER,
   HL_MATCH
@@ -66,6 +68,7 @@ struct editorSyntax {
 
   char *filetype;
   char **filematch;
+  char **keywords;
   char *single_line_comment_start;
   int flags;
 
@@ -105,11 +108,19 @@ struct editorConfig E;
 /*~~~~~~~~~~~~~~~~~~~~ filetypes ~~~~~~~~~~~~~~~~~~~*/
 
 char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL};
+char *C_HL_keywords[] = {
+  "switch", "if", "while", "for", "break", "continue", "return", "else",
+  "struct", "union", "typedef", "static", "enum", "class", "case",
+
+  "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
+  "void|", NULL
+};
 
 struct editorSyntax HLDB[] = {
   {
     "c",
     C_HL_extensions,
+    C_HL_keywords,
     "//",
     HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
   },
@@ -274,6 +285,8 @@ void editorUpdateSyntax(erow *row) {
   
   if (E.syntax == NULL) return;
   
+  char **keywords = E.syntax->keywords;
+  
   char *scs = E.syntax->single_line_comment_start;
   int scs_len = scs ? strlen(scs) : 0;
 
@@ -323,6 +336,26 @@ void editorUpdateSyntax(erow *row) {
         continue;
       }
     }
+    
+    if (prev_sep) {
+      int j;
+      for (j = 0; keywords[j]; ++j) {
+        int klen = strlen(keywords[j]);
+        int kw2 = keywords[j][klen - 1] == '|';
+        if (kw2) klen--;
+        
+        if (!strncmp(&row->render[i], keywords[j], klen) &&
+            is_seperator(row->render[i + klen])) {
+          memset(&row->hl[i], kw2 ? HL_KEYWORD2 : HL_KEYWORD1, klen);
+          i += klen;
+          break;
+        }
+      }
+      if (keywords[j] != NULL) {
+        prev_sep = 0;
+        continue;
+      }
+    }
 
     prev_sep = is_seperator(c);
     i++;
@@ -334,6 +367,12 @@ int editorSyntaxToColor(int hl) {
   switch (hl) {
   case HL_COMMENT:
     return 36;
+    
+  case HL_KEYWORD1:
+    return 33;
+
+  case HL_KEYWORD2:
+    return 32;
 
   case HL_STRING:
     return 35;
@@ -777,12 +816,16 @@ void editorDrawRows(struct abuf *ab) {
       int current_color = -1;
       int j;
       for (j = 0; j < len; ++j) {
-        if (hl[j] == HL_NORMAL) {
+        if (iscntrl(c[j])) {
+          char sym = (c[j] <= 26) ? '@' + c[j] : '?';
+          abAppend(ab, "\x1b[7m", 4);
+          abAppend(ab, &sym, 1);
+          abAppend(ab, "\x1b[m", 3);
+        } else if (hl[j] == HL_NORMAL) {
           if (current_color != -1) {
             abAppend(ab, "\x1b[39m", 5);
             current_color = -1;
           }
-
           abAppend(ab, &c[j], 1);
         } else {
           int color = editorSyntaxToColor(hl[j]);
